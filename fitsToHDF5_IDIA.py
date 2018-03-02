5eb1c1f6ce9b4e8ec1d3c35c9b556dccdfe8ac63
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-03-02 13:43:48 +0200

    works for basic stats

--- fitsToHDF5_IDIA.py
+++ fitsToHDF5_IDIA.py
@@ -15,7 +15,8 @@ class DataSet:
         else:
             self.name = "DATA"
         
-        self.axes = axes        
+        self.axes = axes
+        self._reverse_axes = list(reversed(axes))
         self.data = data
     
     # TODO axes object
@@ -27,13 +28,13 @@ class DataSet:
         #if isinstance(axis_numeric, int):
             #axis_numeric = (axis_numeric,)
         
-        #return "".join(sorted(reversed(self.axes)[d] for d in axis_numeric))
+        #return "".join(sorted(self._reverse_axes[d] for d in axis_numeric))
         
     def axis_numeric(self, axis_name):
         """Convert named axes to numeric axes, relative to this dataset
             e.g. if axes are XYZW, Z -> 1, XY -> (2, 3)
         """
-        axis_numeric = tuple(sorted(reversed(self.axes).index(l) for l in axis_name))
+        axis_numeric = tuple(sorted(self._reverse_axes.index(l) for l in axis_name))
         
         if len(axis_numeric) == 1:
             return axis_numeric[0]
@@ -46,13 +47,13 @@ class DataSet:
         axis = self.axis_numeric(axis_name)
         
         with warnings.catch_warnings():
-            # nanmean prints a warning for empty slices, i.e. planes full of nans, but gives the correct answer (nan).
+            # nanmean, etc. print a warning for empty slices, i.e. planes full of nans, but give the correct answer (nan).
             warnings.simplefilter("ignore", category=RuntimeWarning)
-            stats.create_dataset("MEANS", np.nanmean(self.data, axis=axis))
-            stats.create_dataset("MIN_VALS", np.nanmin(self.data, axis=axis))
-            stats.create_dataset("MAX_VALS", np.nanmax(self.data, axis=axis))
+            stats.create_dataset("MEAN", data=np.nanmean(self.data, axis=axis))
+            stats.create_dataset("MIN", data=np.nanmin(self.data, axis=axis))
+            stats.create_dataset("MAX", data=np.nanmax(self.data, axis=axis))
             
-        stats.create_dataset("NAN_COUNTS", np.count_nonzero(np.isnan(self.data), axis=axis))
+        stats.create_dataset("NAN_COUNT", data=np.count_nonzero(np.isnan(self.data), axis=axis))
         
     #def write_histogram(self, path, data, axis):
         ## TODO make this generic; able to work with any data and axis
@@ -101,7 +102,7 @@ class DataSet:
     
     def write(self, hdu_group, statistics_axes, chunks):        
         # write this dataset
-        hdu_group.create_dataset(self.name, data=self.data, chunks=chunks)
+        hdu_group.create_dataset(self.name, data=self.data, chunks=tuple(chunks) if chunks else None)
         
         # write statistics
         for axis_name in statistics_axes:
@@ -117,23 +118,23 @@ class HDUGroup:
     # FITS header attributes to keep (regular expression matches)
     FITS_KEEP_RE = (
         re.compile('^(CDELT|CROTA|CRPIX|CRVAL|CTYPE|CUNIT)\d+'),
+        re.compile('^NAXIS\d*')
     )
     
     def __init__(self, name, fits_hdu):
         self.name = name
         self.data = fits_hdu.data
         self.header = fits_hdu.header
-        
-    @staticmethod
-    def convert(val):
-        if isinstance(val, str):
-            return np.string_(val)
-        return val
 
     def copy_attrs(self, hdu_group, keys):
+        def _convert(val):
+            if isinstance(val, str):
+                return np.string_(val)
+            return val
+        
         for key in keys:
             if key in self.header:
-                hdu_group.attrs.create(key, Converter.convert(self.header[key]))
+                hdu_group.attrs.create(key, _convert(self.header[key]))
         
     def write(self, hdf5file, statistics_axes=None, chunks=None):
         hdu_group = hdf5file.require_group(self.name)
@@ -151,10 +152,13 @@ class HDUGroup:
         if 'COMMENT' in self.header:
             hdu_group.create_dataset('COMMENT', [np.string_(val) for val in self.header['COMMENT']])
             
-        # TODO get the actual axes out of the header
-        main_dataset = DataSet(self.data, "XYZW")
+        num_axes = self.header["NAXIS"]
         
-        # TODO get statistics axes from args
+        # TODO: this is good enough for now, but we should look for a more robust way to detect the order
+        axes = "XYZW"[:num_axes]
+        
+        main_dataset = DataSet(self.data, axes)
+        
         main_dataset.write(hdu_group, statistics_axes=statistics_axes, chunks=chunks)
         
         # TODO: swizzles
@@ -177,8 +181,9 @@ class Converter:
 
     def convert(self, args):
         # TODO: iterate over all the HDUs -- how do we pick the names?
+        # TODO: some HDUs are just tables.
         primary = HDUGroup("Primary", self.fits[0])
-        primary.write(self.hdf5, statistics_axes=("XY", "Z", "XYZ"), chunks=tuple(args.chunks) if args.chunks else None)
+        primary.write(self.hdf5, statistics_axes=args.statistics_axes, chunks=args.chunks)
 
 
 if __name__ == '__main__':
@@ -186,6 +191,9 @@ if __name__ == '__main__':
     parser.add_argument('filename', help='Input filename')
     # TODO how do we change this when we keep the 4D dataset?
     parser.add_argument('--chunks', nargs=3, type=int, help='Chunks to use, order: Z Y X')
+    parser.add_argument('--statistics-axes', nargs="+", help='Axes along which statistics should be calculated, e.g. XY, Z, XYZ')
+    # TODO if we want to split out stokes, we should pass in a stokes parameter
+    # TODO optional parameter to override axes?
     args = parser.parse_args()
     
     baseFileName, _ = os.path.splitext(args.filename)
