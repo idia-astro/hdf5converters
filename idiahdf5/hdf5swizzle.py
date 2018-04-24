ac0a19c89dfc16a8827d688f96cbe8546f74a589
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-04-24 12:21:15 +0200

    more work in progress

--- idiahdf5/hdf5swizzle.py
+++ idiahdf5/hdf5swizzle.py
@@ -1,7 +1,9 @@
 import logging
 import numpy as np
 import h5py
+import subprocess
 
+# TODO simplify; merge this back into swizzler class
 class Dataset:
     def __init__(self, hdu_group, data, axes):
         self.hdu_group = hdu_group
@@ -34,13 +36,12 @@ class Dataset:
 
 
 class Swizzler:
-    PARALLEL = ('ZYXW',)
-    
     def __init__(self, hdf5name):
         self.hdf5name = hdf5name
         
     def __enter__(self):
         self.hdf5 = h5py.File(self.hdf5name, "r+")
+        self.data = self.hdf5["0"]["DATA"]
         return self
         
     def __exit__(self, e_type, e_value, e_traceback):
@@ -48,17 +49,57 @@ class Swizzler:
     
 
     def swizzle(self, args):
-        primary_group = self.hdf5["0"]
-        data = primary_group["DATA"]
         
-        swizzled_group = primary_group.require_group("SwizzledData")
         
         # TODO: this is good enough for now, but we should look for a more robust way to detect the order
         axes = "XYZW"[:len(data.shape)]
         reversed_data_axes = list(reversed(axes))
         
         for axis_name in args.swizzles:
-            if not args.parallel or axis_name not in self.PARALLEL:
+            swizzled_group = self.hdf5["0"].require_group("SwizzledData")
+            if not SwizzlerParallel.implemented(data, axis_name):
+                pass # ??
+                
+                
+                
+class SwizzlerParallel(Swizzler):
+    @classmethod
+    def implemented(cls, data, swizzled_axes):
+        # TODO: only one case for now; may extend later
+        if data.ndim == 4 and data.shape[0] > 1 and swizzled_axes = "ZYXW":
+            return True
+        return False
+    
+    def __enter__(self):
+        from mpi4py import MPI
+        self.hdf5 = h5py.File(filename, 'r+', driver='mpio', comm=MPI.COMM_WORLD)
+        self.data = self.hdf5["0"]["DATA"]
+        self.rank = MPI.COMM_WORLD.rank
+        return self
+    
+    def swizzle_ZYXW(self):
+        W, Z, Y, X = self.data.shape
+        
+        swizzled_group = self.hdf5["0"].require_group("SwizzledData")
+        swizzled_dataset = swizzled_group.create_dataset("ZYXW", (W, X, Y, Z), dtype=self.data.dtype)
+        
+        data_w = np.empty((Z, Y, X), dtype=self.data.dtype)
+        
+        w = self.rank
+        
+        # read one z at a time
+        for z in range(Z):
+            data_w[z] = self.data[w,z,:,:]
+            
+        # swizzle whole w by transposing
+        swizzled_data_w = np.transpose(data_w, (2, 1, 0))
+        
+        # write one x at a time
+        for x in range(X):
+            swizzled_dataset[w, x, :, :] = swizzled_data_w[x, :, :]
+            
+    def swizzle(self):
+        pass
                 
 
 def swizzle(args):
--- idiahdf5/hdf5swizzle_parallel.py
+++ idiahdf5/hdf5swizzle_parallel.py
@@ -1,9 +1,1 @@
-import logging
-import numpy as np
-import h5py
-
-from hdf5swizzle import Swizzler
-
-class SwizzlerParallel(Swizzler):
-    pass
-    
+# TODO: DELETE THIS
